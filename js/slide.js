e5634a7f60bc16c609288ac5e6d87805f3d356ce
jihkyuo <84238602+jihkyuo@users.noreply.github.com>
2021-09-28 23:15:16 +0900

    nail content plus

--- js/slide.js
+++ js/slide.js
@@ -1,5 +1,7 @@
 const SHOW = "showing";
+const SHOW_NAIL = "showingNail";
 const ctrlBtn = document.querySelectorAll(".control__item");
+const ctrlNailBtn = document.querySelectorAll(".control-nail__item");
 
 function showChange(i) {
   let item = document.querySelectorAll(".main__product");
@@ -17,5 +19,24 @@ function showChange(i) {
 for (let i = 0; i < ctrlBtn.length; i++) {
   ctrlBtn[i].addEventListener("click", () => {
     showChange(i);
+  });
+}
+
+function NailShowChange(i) {
+  let item = document.querySelectorAll(".main__product__nail");
+  const currentShow = document.querySelector(".showingNail");
+  currentShow.classList.remove(SHOW_NAIL);
+  item[i].classList.add(SHOW_NAIL);
+  console.log(item);
+  console.log(i);
+  for (let idx = 0; idx < ctrlNailBtn.length; idx++) {
+    ctrlNailBtn[idx].classList.remove("paintBtn");
+  }
+  ctrlNailBtn[i].classList.add("paintBtn");
+}
+
+for (let i = 0; i < ctrlNailBtn.length; i++) {
+  ctrlNailBtn[i].addEventListener("click", () => {
+    NailShowChange(i);
   });
 }
